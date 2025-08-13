76be4fcd39db96234661902d3f4c93eebd3902db
franabad <fam1096@hotmail.com>
2025-08-13 04:01:37 +0200

    Add @Data annotation to AccountModel class

--- accounts-service/src/main/java/com/project/accounts_service/model/AccountModel.java
+++ accounts-service/src/main/java/com/project/accounts_service/model/AccountModel.java
@@ -1,9 +1,11 @@
 package com.project.accounts_service.model;
 
 import jakarta.persistence.Entity;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@Data
 public class AccountModel {
 }
