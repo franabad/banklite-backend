c7233e5706c58e3c8a33d7c57666cd452d7cdbcb
franabad <fam1096@hotmail.com>
2025-08-13 04:02:44 +0200

    Add @AllArgsConstructor annotation to AccountModel class

--- accounts-service/src/main/java/com/project/accounts_service/model/AccountModel.java
+++ accounts-service/src/main/java/com/project/accounts_service/model/AccountModel.java
@@ -1,11 +1,13 @@
 package com.project.accounts_service.model;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class AccountModel {
 }
